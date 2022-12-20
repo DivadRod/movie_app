1a1a67d2d2ef257b8fb54af433b8778e92c1c31a
David R <davrodzam@gmail.com>
2022-12-20 15:33:40 -0600

    MovieDetails - Slivers - SliverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,40 +1,46 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-
-    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
 
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          _CustomAppBar(),
-        ],
-      )
-    );
+        body: CustomScrollView(
+      slivers: [
+        _CustomAppBar(),
+      ],
+    ));
   }
 }
 
 class _CustomAppBar extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return const SliverAppBar(
+    return SliverAppBar(
       backgroundColor: Colors.blue,
       expandedHeight: 200,
       floating: false,
       pinned: true,
       flexibleSpace: FlexibleSpaceBar(
-          centerTitle: true,
-          title: Text('movie.title'),
-          background: FadeInImage(
-            placeholder: AssetImage('assets/loading.gif'),
-             image:AssetImage('assets/no-image.jpg'),
-             fit: BoxFit.cover,),
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+            width: double.infinity,
+            alignment: Alignment.bottomCenter,
+            color: Colors.black12,
+            child: const Text(
+              'movie.title',
+              style: TextStyle(fontSize: 16),
+            )),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage(
+              'https://www.latercera.com/resizer/5dA2h81bp0qtf47WXlyA31ctnAE=/800x0/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/T2OBTBO36BFPLPM3WVQSATJQ3I.jpg'),
+          fit: BoxFit.cover,
         ),
+      ),
     );
   }
 }
